379c8f596474a66c4c8d809624938f3384a72b09
Alex Serdyuk <aserdyuk@dius.com.au>
2016-09-15 15:37:17 +1000

    more logging

--- app.rb
+++ app.rb
@@ -5,6 +5,7 @@ require_relative 'weather_reader'
 post '/gateway' do
   weather_bot = WeatherReader.new
   message = params[:text].gsub(params[:trigger_word], '').strip
+  puts "--------------------- received message: #{message}"
   result = weather_bot.process_command(message)
 
   content_type :json
